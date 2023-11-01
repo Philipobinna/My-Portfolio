bc9042ccddc10d0449e294f4df15785bab7f2195
philipObinna <philip.obinnakwelu@decagon.dev>
2023-11-01 20:22:24 +0100

    my portfolio

---
+++ src/data.js
@@ -0,0 +1,169 @@
+import {
+  CSS3,
+  Express,
+  HTML5,
+  Javascript,
+  Mongodb,
+  Nextjs,
+  Nextjs2,
+  Nodejs,
+  Python,
+  Reactjs,
+  admin,
+  food,
+  food1,
+  port1,
+  port2,
+  port3,
+  social,
+} from "./assets";
+
+export const projectStats = [
+  {
+    name: "Total Projects",
+    value: "10+",
+  },
+  {
+    name: "Website Design",
+    value: "20+",
+  },
+  {
+    name: "Mobile App Design",
+    value: "5",
+  },
+  {
+    name: "Clients worldwide",
+    value: "5+",
+  },
+];
+
+export const tools = [
+  {
+    name: "MongoDB",
+    icon: Mongodb,
+  },
+  {
+    name: "Express",
+    icon: Express,
+  },
+  {
+    name: "React",
+    icon: Reactjs,
+  },
+  {
+    name: "NodeJs",
+    icon: Nodejs,
+  },
+  {
+    name: "NextJs",
+    icon: Nextjs2,
+  },
+];
+
+export const skills = [
+  {
+    name: "ReactJs",
+    value: 80,
+    icon: Reactjs,
+  },
+  {
+    name: "NodeJs",
+    value: 85,
+    icon: Nodejs,
+  },
+  {
+    name: "NextJs",
+    value: 70,
+    icon: Nextjs,
+  },
+  {
+    name: "MongoDB",
+    value: 90,
+    icon: Mongodb,
+  },
+  {
+    name: "CSS3",
+    value: 90,
+    icon: CSS3,
+  },
+  {
+    name: "HTML5",
+    value: 95,
+    icon: HTML5,
+  },
+  {
+    name: "JavaScript",
+    value: 88,
+    icon: Javascript,
+  },
+  {
+    name: "Python",
+    value: 78,
+    icon: Python,
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Admin Dashboard",
+    cat: "Web Design",
+    url: "",
+    img: admin,
+  },
+  {
+    id: 2,
+    title: "Portfolio Website",
+    cat: "Web Design",
+    url: "",
+    img: port1,
+  },
+  {
+    id: 3,
+    title: "Portfolio Website",
+    cat: "Web Design",
+    url: "https://www.livescore.com/en/",
+    img: port2,
+  },
+  {
+    id: 4,
+    title: "MTS Website",
+    cat: "Web Design",
+    url: "",
+    img: port3,
+  },
+  {
+    id: 5,
+    title: "Eat Well Website",
+    cat: "Web Design",
+    url: "",
+    img: food,
+  },
+  {
+    id: 6,
+    title: "Social Media",
+    cat: "Web App",
+    url: "",
+    img: social,
+  },
+  {
+    id: 7,
+    title: "Eat Well App",
+    cat: "Mobile App",
+    url: "",
+    img: food1,
+  },
+];
+
+export const softskills = [
+  "Time management",
+  "Communication",
+  "Empathy",
+  "Teamwork",
+  "Helpfulness",
+  "Creativity",
+  "Self-learning",
+  "Accountability",
+  "Problem solving",
+  "Open-mindedness",
+];
